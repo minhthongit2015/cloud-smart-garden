ae504969096f61a57f7559d4a01acae9796f05fe
Minh Thong <thongnmtran@kms-technology.com>
2019-08-27 22:01:26 +0700

    update

--- src/server/websocket/routes/gardens/stations/index.js
+++ src/server/websocket/routes/gardens/stations/index.js
@@ -7,7 +7,7 @@ const debug = DebugHelper(Debug.ws.ROUTING);
 router.post('/state', async (req, res) => {
   debug('Station state: ', req.sessionID,
     req.session.user ? req.session.user.username || req.session.user : '');
-  console.log(req.body);
+  // console.log(req.body);
 });
 
 module.exports = router;
